6dbd4ea08c8652724e382107f5b58eefd7328fca
Your Name <you@example.com>
2021-01-28 14:30:39 +0300

    lab16

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let http = require("http");
-var generateImage = require('random-image-creator');
-http.createServer(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    let pict = generateImage(280, 400)
-    response.write("<img src="+pict+">");
-response.end();
+const http = require("http");
+const generateImage = require("random-image-creator");
+
+http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  const pict = generateImage(280, 400);
+  response.write("<img src="+pict+">");
+  response.end();
 }).listen(3000);
